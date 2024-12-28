ff4e66a161680982133cafdafbd9466f7d44df12
Andrew Vabishchevich <144654863+AndrewVabishchevichK3123@users.noreply.github.com>
2024-12-28 14:20:47 +0300

    Update test00.py

--- lab03/tests/test00.py
+++ lab03/tests/test00.py
@@ -1,14 +1,9 @@
-import unittest
-from app.main import add, subtract
+def add(a, b):
+    return a + b
 
-class TestMathOperations(unittest.TestCase):
-    def test_add(self):
-        self.assertEqual(add(1, 2), 3)
-        self.assertEqual(add(-2, 1), -1)
+def subtract(a, b):
+    return a - b
 
-    def test_subtract(self):
-        self.assertEqual(subtract(2, 1), 1)
-        self.assertEqual(subtract(0, 0), 0)
-
-if __name__ == '__main__':
-    unittest.main()
+if __name__ == "__main__":
+    print("2 + 1 =", add(2, 1))
+    print("2 - 1 =", subtract(2, 1))
