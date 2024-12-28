838cf9690199ab18d024f52986e072ada6cf467c
Andrew Vabishchevich <144654863+AndrewVabishchevichK3123@users.noreply.github.com>
2024-12-28 14:21:28 +0300

    Update test00.py

--- lab03/tests/test00.py
+++ lab03/tests/test00.py
@@ -5,5 +5,5 @@ def subtract(a, b):
     return a - b
 
 if __name__ == "__main__":
-    print("2 + 1 =", add(2, 1))
-    print("2 - 1 =", subtract(2, 1))
+    print("5 + 7 =", add(5, 7))
+    print("7 - 5 =", subtract(7, 5))
